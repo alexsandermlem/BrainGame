7bda6fd45d876297b1ae35fd02db0c62b5745ef2
alexsandermlem <alexsandermlem@gmail.com>
2022-04-12 01:22:27 +0700

    Add core

---
+++ Assets/_Root/Scripts/Controller/PopupController.cs
@@ -0,0 +1,73 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Linq;
+
+public class PopupController : Singleton<PopupController>
+{
+    public Transform CanvasTransform;
+    public CanvasScaler CanvasScaler;
+    public Camera Camera;
+    public Popup[] Popups;
+
+    private Dictionary<Type, Popup> dictionary = new Dictionary<Type, Popup>();
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        DontDestroyOnLoad(gameObject);
+        Init();
+        CanvasScaler.matchWidthOrHeight = Camera.main.aspect > .7f ? 1 : 0;
+    }
+
+    public void Init()
+    {
+        int index = 0;
+        Popups.ToList().ForEach(popup =>
+        {
+            Popup popupInstance = Instantiate(popup, CanvasTransform);
+            popupInstance.gameObject.SetActive(false);
+            popupInstance.Init();
+            popupInstance.Canvas.sortingOrder = popupInstance.Canvas.sortingOrder + index++;
+            popupInstance.CanvasGroup.alpha = 0;
+            popupInstance.CanvasGroup.interactable = false;
+            dictionary.Add(popupInstance.GetType(), popupInstance);
+        });
+    }
+
+    public void Show<T>(object data = null)
+    {
+        if (dictionary.TryGetValue(typeof(T), out Popup popup))
+        {
+            popup.data = data;
+            popup.Show();
+        }
+    }
+
+    public void Hide<T>()
+    {
+        if (dictionary.TryGetValue(typeof(T), out Popup popup))
+        {
+            popup.Hide();
+        }
+    }
+
+    public void HideAll()
+    {
+        foreach (var item in dictionary)
+        {
+            item.Value.Hide();
+        }
+    }
+
+    public T GetPopup<T>() where T : Popup
+    {
+        if (dictionary.TryGetValue(typeof(T), out Popup popup))
+        {
+            return (T)popup;
+        }
+        return default;
+    }
+}
---
+++ Assets/_Root/Scripts/Popup/Popup.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using DG.Tweening;
+
+[RequireComponent(typeof(CanvasGroup), typeof(Canvas))]
+public class Popup : MonoBehaviour
+{
+    public CanvasGroup CanvasGroup { get; set; }
+    public Canvas Canvas { get; set; }
+    public object data;
+
+    public void Init()
+    {
+        CanvasGroup = GetComponent<CanvasGroup>();
+        Canvas = GetComponent<Canvas>();
+        AfterInstantiate();
+    }
+
+    public void Show()
+    {
+        BeforeShow();
+        gameObject.SetActive(true);
+        CanvasGroup.DOFade(1, .3f).OnComplete(() =>
+        {
+            CanvasGroup.interactable = true;
+            AfterShown();
+        });
+    }
+
+    public void Hide()
+    {
+        BeforeHide();
+        CanvasGroup.interactable = false;
+        CanvasGroup.alpha = 0;
+        gameObject.SetActive(false);
+        AfterHidden();
+    }
+
+    protected virtual void AfterInstantiate() { }
+    protected virtual void BeforeShow() { }
+    protected virtual void AfterShown() { }
+    protected virtual void BeforeHide() { }
+    protected virtual void AfterHidden() { }
+}
